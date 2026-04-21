c21418d5fd95cc62ff24ba58343415695068105c
Ajit Pandey <ajitpandey4572@gmail.com>
2026-04-21 22:58:09 +0530

    exp-5 updated

--- AJ/src/App.jsx
+++ AJ/src/App.jsx
@@ -1,39 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Greeting from './Greeting'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-  
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  const reset = () => {
-    setCount(0);
-  };
-
+  const increment = () => setCount(prev => prev + 1);
+  const decrement = () => setCount(prev => prev - 1);
+  const reset = () => setCount(0);
 
   return (
-    <>
-    <h1>React Counter Application</h1>
-    <div className="counter">
-      <h2>Count: {count}</h2>
-      <div className='buttons'>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={reset}>Reset</button>
+    <div className="app">
+
+      <h1>React Counter Application</h1>
+
+      <div className="counter-card">
+        <h2>{count}</h2>
+
+        <div className="buttons">
+          <button onClick={increment}>Increment(+)</button>
+          <button onClick={decrement}>Decrement(−)</button>
+          <button onClick={reset}>Reset</button>
+        </div>
       </div>
-      
+
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
